5bd14409180dfcbcf1cc557121d82d168db6c31f
Sean Mercer <seansmercer@gmail.com>
2019-05-06 14:03:53 -0400

    State passed down

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
 import Login from './Components/Login'
 import Register from './Components/Register'
+import Videos from './Components/Videos'
 import './App.css';
 import { readAllVideos, loginUser, registerUser } from './Services/api-helper';
-import {withRouter} from 'react-router'
 import {Route} from 'react-router-dom'
-import ReactPlayer from 'react-player'
 import decode from 'jwt-decode'
+
 
 class App extends Component {
   constructor(props) {
@@ -23,6 +23,8 @@ class App extends Component {
     this.handleLogin = this.handleLogin.bind(this)
     this.handleRegister = this.handleRegister.bind(this)
     this.authHandleChange = this.authHandleChange.bind(this)
+    this.nextVideo = this.nextVideo.bind(this)
+    this.previousVideo = this.previousVideo.bind(this)
   }
 
   componentDidMount() {
@@ -72,37 +74,40 @@ class App extends Component {
     this.setState({videos})
   }
 
+nextVideo() {
+  let currentVideo = this.state.currentVideo
+  if (currentVideo === (this.state.videos.length - 1)){
+      currentVideo = 0
+      this.setState({currentVideo})} 
+      
+  else {
+      currentVideo++
+      this.setState({currentVideo})
+    }
+  }
+
+previousVideo() {
+  let currentVideo = this.state.currentVideo
+  if (currentVideo > 0){
+      currentVideo--
+      this.setState({currentVideo})} 
+  else {
+    currentVideo = (this.state.videos.length - 1)
+    this.setState({currentVideo})
+  }
+}
+
   render () {
     return (
       <div className="App">
       {this.state.currentUser && <h1>{this.state.currentUser.username}</h1>}
-         {this.state.videos[0] && 
-         <ReactPlayer url={this.state.videos[this.state.currentVideo].url}  playing={false}/>}
-         <button
-          onClick= {() => {
-            let currentVideo = this.state.currentVideo
-            if (currentVideo === (this.state.videos.length - 1)){
-              currentVideo = 0
-              this.setState({currentVideo})
-            } else {
-              currentVideo++
-              this.setState({currentVideo})
-            }
-          }}
-         >Next video</button>
-         <button
-          onClick= {() => {
-            let currentVideo = this.state.currentVideo
-            if (currentVideo > 0){
-            currentVideo--
-            this.setState({currentVideo})
-            } else {
-              currentVideo = (this.state.videos.length - 1)
-              this.setState({currentVideo})
-            }
-          }}
-         >Previous video</button>
-
+        <Route exact path="/videos" render={() => (
+          <Videos
+            videos={this.state.videos}
+            currentVideo={this.state.currentVideo}
+            currentUser={this.state.currentUser}
+            next={this.nextVideo}
+            previous={this.previousVideo} />)} />
         <Route exact path="/login" render={() => (
           <Login
             handleLogin={this.handleLogin}
